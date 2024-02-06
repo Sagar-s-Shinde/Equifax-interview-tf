a7d708edff13c3427284c992716e0d12c292d26e
Sagar-s-Shinde <sagar.it01@gmail.com>
2024-02-06 13:30:30 +0530

    Create multiple resources using for loop

--- instance.tf
+++ instance.tf
@@ -1,12 +1,13 @@
 resource "aws_instance" "eq-inst" {
-  count         = 2
-  ami                    = var.AMIS[var.REGION]
-  instance_type          = "t2.micro"
-  availability_zone      = var.ZONE1
-  key_name               = "Test-Key"
-  vpc_security_group_ids = ["sg-0780815f55104be8a"]
-  tags = {
-    Name = "eq-inst-${count.index + 1}"
+  for i in range(1,3) :{
+    ami                    = var.AMIS[var.REGION]
+    instance_type          = "t2.micro"
+    availability_zone      = var.ZONE1
+    key_name               = "Test-Key"
+    vpc_security_group_ids = ["sg-0780815f55104be8a"]
+    tags = {
+        Name = "eq-inst-${i}"
+    }
   }
 }
 
