1a48b67f53546bea11fe5d2fc19af130b95d44ce
Sagar-s-Shinde <sagar.it01@gmail.com>
2024-02-06 12:35:34 +0530

    multiple instance with count

--- instance.tf
+++ instance.tf
@@ -1,11 +1,16 @@
 resource "aws_instance" "eq-inst" {
+  count         = 2
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
   key_name               = "Test-Key"
   vpc_security_group_ids = ["sg-0780815f55104be8a"]
+  tags = {
+    Name = "eq-inst-${count.index + 1}"
+  }
 }
 
 output "PublicIP" {
-  value = aws_instance.eq-inst.public_ip
+  value = [for instance in aws_instance.eq-inst : instance.public_ip]
+
 }
